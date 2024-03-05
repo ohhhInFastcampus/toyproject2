44c0b5a5a0408c45268397dedfd415b06cc73f0d
kano <ajvls98@gmail.com>
2024-03-05 11:57:14 +0900

    add: env검증

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { z } from "zod";
 
-const firebaseConfig = {
+const firebaseConfigSchema = z.object({
+  apiKey: z.string(),
+  authDomain: z.string(),
+  projectId: z.string(),
+  storageBucket: z.string(),
+  messagingSenderId: z.string(),
+  appId: z.string(),
+});
+
+const firebaseConfig = firebaseConfigSchema.parse({
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
-};
+});
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
