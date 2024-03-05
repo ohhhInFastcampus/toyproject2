54c52b7f4f9bb1303394a9db735c4a427c955edc
Hong Joo Choi <hchoi2894@gmail.com>
2024-03-05 11:30:54 +0900

    TP-32 #comment 헤더 생성

---
+++ src/app/header.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+const Header = () => {
+  return (
+    <div>
+      <Image src="/next.svg" alt="profile picture" width={100} height={100} />
+    </div>
+  );
+};
+export default Header;
