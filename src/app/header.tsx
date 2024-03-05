545fb87ec5cbb66df4c3b05dae93ed1d6ef3e9ae
Hong Joo Choi <hchoi2894@gmail.com>
2024-03-05 12:39:23 +0900

    TP-35 #in-progress #comment gnb 추가

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,6 +3,16 @@
 import Image from "next/image";
 import User from "./User";
 import styled from "styled-components";
+import Link from "next/link";
+
+const Gnb = () => {
+  return (
+    <>
+      <Link href="/">HOME</Link>
+      <Link href="/pay">급여내역</Link>
+    </>
+  );
+};
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -14,6 +24,7 @@ const Header = () => {
   return (
     <HeaderContainer>
       <Image src="/next.svg" alt="profile picture" width={100} height={100} />
+      <Gnb />
       <User />
     </HeaderContainer>
   );
