aeb06d45d44d86f1adc29185ad3546e81b7f32aa
kano <ajvls98@gmail.com>
2024-03-06 18:48:21 +0900

    add: nextjs 미들웨어추가

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  console.log("middleware");
+
+  // 로그인 상태 확인, 쿠키 사용
+  const isLoggedIn = request.cookies.get("token");
+  console.log("isLoggedIn", isLoggedIn);
+  console.log("pathname", pathname);
+
+  // 로그인 페이지 접근 시 로그인 상태에 따라 처리
+  if (pathname === "/login") {
+    if (isLoggedIn) {
+      console.log("로그인상태입니다");
+      return NextResponse.redirect(new URL("/pay", request.url));
+    } else {
+      console.log("로그인상태가 아닙니다");
+      return NextResponse.next();
+    }
+  }
+
+  // 로그인 상태가 아니고 로그인 페이지가 아닌 다른 페이지에 접근 시 /login으로 리디렉션
+  if (!isLoggedIn && pathname !== "/login") {
+    console.log(
+      "로그인 상태가 아니고 로그인 페이지가 아닌 다른 페이지에 접근 시 /login으로 리디렉션"
+    );
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+};
