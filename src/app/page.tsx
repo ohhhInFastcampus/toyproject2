5af3d5148641e73951d39c0fa3eccb8af1dfb328
chloeun <chloekje22@gmail.com>
2024-03-12 16:42:52 +0900

    build 오류 수정 - Calendar component 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Calendar from "@/components/Calendar/Calendar";
 import Header from "@/components/header";
+import Calendar from "@/app/(afterLogin)/calendar/page";
 
 export default function Home() {
   return (
