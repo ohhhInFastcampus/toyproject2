8e602551d49ec797fd656a7b8fdce951c4b5eeb4
kano <ajvls98@gmail.com>
2024-03-12 10:51:46 +0900

    style: 스타일 요소 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    lightGrey: "#F9F9F9",
+    white: "#FFFFFF",
+    darkGray: "#333333",
+    blue: "#3366FF",
+    brightBlue: "#F1F7FE",
+  },
+  border: {
+    radius: "10rem",
+  },
+};
+
+export default theme;
